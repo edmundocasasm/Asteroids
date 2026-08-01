334335fa017d9115a0f3b4abb30928d7a7bda05e
edmundocasasm <edmundo.casas.m@gmail.com>
2026-08-01 02:06:46 -0500

    A: Initial Commit, adding logger.py and constants.py

---
+++ main.py
@@ -0,0 +1,9 @@
+import pygame, constants
+
+def main():
+    print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
+    print(f"Screen width: {constants.SCREEN_WIDTH}")
+    print(f"Screen height: {constants.SCREEN_HEIGHT}")
+    
+if __name__ == "__main__":
+    main()
