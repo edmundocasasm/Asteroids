3503bb3804a9f8eb642d1ba008768560879326e0
edmundocasasm <edmundo.casas.m@gmail.com>
2026-08-02 23:12:37 -0500

    M: Adding splitting, doing some cleanwork with constants

--- asteroid.py
+++ asteroid.py
@@ -1,6 +1,7 @@
-import pygame
-from constants import LINE_WIDTH
+import pygame, random
+from constants import *
 from circleshape import CircleShape
+from logger import log_event
 
 class Asteroid(CircleShape):
     def __init__(self, x: int, y: int, radius: float):
@@ -12,3 +13,23 @@ class Asteroid(CircleShape):
     def update(self, dt: float) -> None:
         # Update the position based on velocity and delta time
         self.position += self.velocity * dt
+
+    def split(self) -> None:
+        # Split the asteroid into two smaller asteroids
+        self.kill()  # Remove the current asteroid from the game
+        if self.radius <= ASTEROID_MIN_RADIUS:  # Minimum size to split
+            return
+
+        log_event("asteroid_split")
+        new_angle = random.uniform(20, 50)
+        asteroid_1_vector = self.velocity.rotate(new_angle)
+        asteroid_2_vector = self.velocity.rotate(-new_angle) 
+
+        new_radius = self.radius - ASTEROID_MIN_RADIUS  # Reduce the radius for the new asteroids
+
+        
+        asteroid_1 = Asteroid(self.position.x, self.position.y, new_radius)
+        asteroid_2 = Asteroid(self.position.x, self.position.y, new_radius)
+
+        asteroid_1.velocity = asteroid_1_vector * 1.2
+        asteroid_2.velocity = asteroid_2_vector * 1.2
--- player.py
+++ player.py
@@ -1,8 +1,9 @@
 import circleshape, pygame
+from circleshape import CircleShape
 from constants import *
 from shot import Shot
 
-class Player(circleshape.CircleShape):
+class Player(CircleShape):
     def __init__(self, x, y):
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0.0  # Rotation angle in degrees
